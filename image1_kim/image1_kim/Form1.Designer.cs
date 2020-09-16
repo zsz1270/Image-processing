af1854ff4ee1a9a2f47f925f717dcefe74e78033
zsz1270 <zsz1270@naver.com>
2020-09-16 13:16:14 +0900

    2주차

---
+++ image1_kim/image1_kim/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace image1_kim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.영상처리ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fieToolStripMenuItem,
+            this.영상처리ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fieToolStripMenuItem
+            // 
+            this.fieToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fieToolStripMenuItem.Name = "fieToolStripMenuItem";
+            this.fieToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fieToolStripMenuItem.Text = "file";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // 영상처리ToolStripMenuItem
+            // 
+            this.영상처리ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.greyToolStripMenuItem});
+            this.영상처리ToolStripMenuItem.Name = "영상처리ToolStripMenuItem";
+            this.영상처리ToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.영상처리ToolStripMenuItem.Text = "영상처리";
+            // 
+            // greyToolStripMenuItem
+            // 
+            this.greyToolStripMenuItem.Name = "greyToolStripMenuItem";
+            this.greyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.greyToolStripMenuItem.Text = "grey";
+            this.greyToolStripMenuItem.Click += new System.EventHandler(this.greyToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem 영상처리ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greyToolStripMenuItem;
+    }
+}
+
---
+++ image1_kim/image1_kim/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace image1_kim
+{
+    public partial class Form1 : Form
+    {
+        Image image;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Graphics gr = CreateGraphics();
+            openFileDialog1.Title = "영상 파일 열기";
+            openFileDialog1.Filter = "ALL Files(*.*) |*.*| Bitmap File(*.bmp)|*bmp |Jpeg File(*.jpg) | *jpg";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string strFilename = openFileDialog1.FileName;
+                image = Image.FromFile(strFilename);
+                gr.DrawImage(image, 30, 80, image.Width, image.Height);
+            }
+            gr.Dispose();
+        }
+
+        private void greyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int x, y, brightness;
+            Color color, gray;
+            Graphics gr = CreateGraphics();
+            Bitmap gBitmap = new Bitmap(image);
+            for(x = 0; x< image.Width; x++)
+            {
+                for(y=0; y<image.Height; y++)
+                {
+                    color = gBitmap.GetPixel(x, y);
+                    brightness = (int)(0.299 * color.R + 0.587 * color.G + 0.114 * color.B);
+                    gray = Color.FromArgb(brightness, brightness, brightness);
+                    gBitmap.SetPixel(x, y, gray);
+                }
+            }
+            gr.DrawImage(gBitmap, 40 + gBitmap.Width, 80, gBitmap.Width, gBitmap.Height);
+            gr.Dispose();
+        }
+    }
+}
